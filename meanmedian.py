841a0bf038861b7f3f1e4e667e3a1e889841a679
Rockwell_591 <elaina.rockwell@gisaoh.org>
2026-02-28 23:16:14 -0500

    Update meanmedian.py

--- meanmedian.py
+++ meanmedian.py
@@ -4,3 +4,19 @@
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
 # video url: https://youtu.be/VGrRdUunjXg
+
+numbers = []
+for i in range(5):
+    number = int(input("Enter a number: "))
+    numbers.append(number)
+print("As entered: ")
+print(numbers)
+print("Small to large: ")
+print(sorted(numbers))
+print("Large to small: ")
+print(sorted(numbers, reverse = True))
+Mean = sum(numbers) / 5 
+print("median:", Mean)
+sorted_numbers = sorted(numbers) 
+median = sorted_numbers[2]
+print("median:", median)
